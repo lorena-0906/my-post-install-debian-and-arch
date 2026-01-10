8d40cb5188c4e662c628edbd6b3948871a35618c
lorena-0906 <charlielucca2906@gmail.com>
2026-01-10 17:08:46 -0300

    idk......

---
+++ debian.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+
+echo "hello, this script install multiple tools, include: fish-shell, kitty, git, curl and others disk tools :) install and enable cloudflare dns"
+
+sudo apt install kitty fastfetch flatpak git curl  zenity fish gufw discover vte3 python-requests gnome-disk-utility bashtop baobab wget ncdu ufw code 
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+clear 
+
+flatpak install vesktop spotify warehouse flatsweep flatseal bitwarden
+
+curl -fsSL https://linux.toys/install.sh | bash
+
+clear
+
+sudo apt update && sudo apt upgrade 
+
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+sudo apt update && sudo apt install cloudflared
+
+warp-cli registration new
+
+warp-cli connect
---
+++ endeavour.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+echo "please view code of this script before your install."
+
+sudo pacman -S kitty fastfetch flatpak git curl  zenity fish gufw discover vte3 python-requests btrfs-assistant snapper gnome-disk-utility htop baobab wget ncdu ufw code 
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+curl -fsSL https://linux.toys/install.sh | bash
+
+clear
+
+yay -S cloudflare-warp-bin
+
+clear
+
+sudo pacman -Syu 
+
+sudo systemctl start warp-svc
+
+sudo systemctl enable warp-svc
+
+sudo systemctl enable --now warp-svc.service
+
+warp-cli registration new
+
+warp-cli connect
+
+clear 
+
+flatpak install vesktop spotify warehouse flatsweep flatseal bitwarden lact
+
+clear 
+
+chsh -s "$(command -v fish)"
+
